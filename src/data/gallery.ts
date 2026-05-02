394493ebd0f3a41f4b7eb82babf6c83fa764ab5d
Anna Kutova <annnakutova@gmail.com>
2026-05-02 12:18:36 +0200

    perf: optimise pictures load - move pictures from /public to src/assets/ - change paths in .ts files - update .astro pages accordingly

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -12,10 +12,24 @@ export interface GalleryItem {
     year: string;
     material: MultilangText;
     description: MultilangText;
-    src: string;
+    src: ImageMetadata;
 }
 
-const BASE_URL = import.meta.env.BASE_URL;
+import psycho from "../assets/gallery/American Psycho.webp";
+import buzok from "../assets/gallery/Buzok.webp";
+import dorm from "../assets/gallery/Dorm.webp";
+import duck from "../assets/gallery/Duck.webp";
+import monkey from "../assets/gallery/Even Monkey Knows.webp";
+import hills from "../assets/gallery/Hills.webp";
+import cats from "../assets/gallery/Japanese Cats.webp";
+import mark from "../assets/gallery/Mark.webp";
+import mushroom from "../assets/gallery/Mushroom.webp";
+import panels from "../assets/gallery/Panels.webp";
+import peach from "../assets/gallery/Peach.webp";
+import siauliai from "../assets/gallery/Siauliai.webp";
+import tangerine from "../assets/gallery/Tangerine.webp";
+import taxi from "../assets/gallery/Yellow Taxi.webp";
+
 
 export const galleryIntro: MultilangText = {
     en: `I started painting with gouache in the first grade while attending art classes after school, but I stopped by the second grade. 
@@ -44,10 +58,10 @@ Normalmente prefiero pintar con acrílicos, ¡pero a veces me gusta probar mater
 export const galleryItems: GalleryItem[] = [
     {
         id: '1',
-        title: { en: 'American Psycho', uk: 'Американський психопат', es: 'American Psycho' },
-        src: `${BASE_URL}gallery/American Psycho.webp`,
+        title: {en: 'American Psycho', uk: 'Американський психопат', es: 'American Psycho'},
+        src: psycho,
         year: '2023',
-        material: { en: 'Acrylics', uk: 'Акрил', es: 'Acrílicos' },
+        material: {en: 'Acrylics', uk: 'Акрил', es: 'Acrílicos'},
         description: {
             en: `I think the title speaks for itself. After watching the movie "American Psycho," I was really struck by its aesthetic. \n\nAt the time, "sigma" memes were everywhere, which inspired me to paint Patrick Bateman. It was also a great opportunity to practice my portrait techniques.`,
             uk: `Думаю, назва говорить сама за себе. Після перегляду фільму "Американський психопат" мене дуже вразила його естетика. \n\nНа той час меми про "сигму" були всюди, що й надихнуло мене намалювати Патріка Бейтмана. Це також була чудова можливість потренувати техніку портрета.`,
@@ -56,10 +70,10 @@ export const galleryItems: GalleryItem[] = [
     },
     {
         id: '2',
-        title: { en: 'Buzok', uk: 'Бузок', es: 'Lilas' },
-        src: `${BASE_URL}gallery/Buzok.webp`,
+        title: {en: 'Buzok', uk: 'Бузок', es: 'Lilas'},
+        src: buzok,
         year: '2020',
-        material: { en: 'Acrylics', uk: 'Акрил', es: 'Acrílicos' },
+        material: {en: 'Acrylics', uk: 'Акрил', es: 'Acrílicos'},
         description: {
             en: `I painted this while being stuck at home during the pandemic. These flowers were a rare piece of the outside world, so I felt a strong urge to capture them. \n\nI used to believe that flowers were easy to paint, but this piece taught me a lot about playing with vases, reflections, and shadows. I still don't like the paintings of flovers.`,
             uk: `Я намалювала це, застрягши вдома під час пандемії. Ці квіти були рідкісним шматочком зовнішнього світу, тому я відчула сильне бажання зафіксувати їх. \n\nРаніше я вірила, що квіти малювати легко, але ця робота навчила мене багато чому у роботі з вазами, відображеннями та тінями. Я досі не дуже люблю картини з квітами.`,
@@ -68,10 +82,10 @@ export const galleryItems: GalleryItem[] = [
     },
     {
         id: '3',
-        title: { en: 'Dorm', uk: 'Гуртожиток', es: 'Residencia' },
-        src: `${BASE_URL}gallery/Dorm.webp`,
+        title: {en: 'Dorm', uk: 'Гуртожиток', es: 'Residencia'},
+        src: dorm,
         year: '2022',
-        material: { en: 'Cheap Acrylics', uk: 'Дешевий акрил', es: 'Acrílicos baratos' },
+        material: {en: 'Cheap Acrylics', uk: 'Дешевий акрил', es: 'Acrílicos baratos'},
         description: {
             en: `When I first moved to Lithuania for my studies, I deeply missed the paints I had left behind at home. \n\nI eventually bought some cheap acrylics from JYSK and started painting what I saw around me. Currently, this painting belongs to a my friend and former classmate.`,
             uk: `Коли я вперше переїхала до Литви на навчання, я дуже сумувала за фарбами, які залишила вдома. \n\nЗрештою я купила дешевий акрил у JYSK і почала малювати те, що бачила навколо. Зараз ця картина належить моєму другові та колишньому однокласнику.`,
@@ -80,10 +94,10 @@ export const galleryItems: GalleryItem[] = [
     },
     {
         id: '4',
-        title: { en: 'Goose', uk: 'Гусак', es: 'Ganso' },
-        src: `${BASE_URL}gallery/Duck.webp`,
+        title: {en: 'Goose', uk: 'Гусак', es: 'Ganso'},
+        src: duck,
         year: '2022',
-        material: { en: 'Acrylics', uk: 'Акрил', es: 'Acrílicos' },
+        material: {en: 'Acrylics', uk: 'Акрил', es: 'Acrílicos'},
         description: {
             en: `I found a similar painting of a goose on Pinterest and felt inspired to recreate it in my sketchbook.`,
             uk: `Я знайшла схожу картину з гусаком на Pinterest і надихнулася відтворити її у своєму скетчбуці.`,
@@ -92,10 +106,10 @@ export const galleryItems: GalleryItem[] = [
     },
     {
         id: '5',
-        title: { en: 'Even Monkey Knows', uk: 'Навіть мавпа знає', es: 'Incluso el mono lo sabe' },
-        src: `${BASE_URL}gallery/Even Monkey Knows.webp`,
+        title: {en: 'Even Monkey Knows', uk: 'Навіть мавпа знає', es: 'Incluso el mono lo sabe'},
+        src: monkey,
         year: '2023-2024',
-        material: { en: 'Acrylics', uk: 'Акрил', es: 'Acrílicos' },
+        material: {en: 'Acrylics', uk: 'Акрил', es: 'Acrílicos'},
         description: {
             en: `This was a "one-day painting" born out of boredom. When I brought it to my parents' house, a friend of theirs absolutely loved the message it conveyed. \n\nHe asked to have it, and although my mum was initially hesitant, I convinced her to let it go. My main philosophy regarding my art is that it has to live. I’m happy knowing that someone truly enjoys it, so we gifted it to him for his birthday.`,
             uk: `Це була "картина одного дня", народжена від нудьги. Коли я привезла її до батьківського дому, їхньому другові дуже сподобався меседж, який вона несла. \n\nВін попросив її собі, і хоча мама спочатку вагалася, я переконала її віддати. Моя головна філософія щодо мистецтва полягає в тому, що воно має жити. Я щаслива знати, що хтось щиро насолоджується нею, тому ми подарували її йому на день народження.`,
@@ -104,10 +118,10 @@ export const galleryItems: GalleryItem[] = [
     },
     {
         id: '6',
-        title: { en: 'Hills', uk: 'Пагорби', es: 'Colinas' },
-        src: `${BASE_URL}gallery/Hills.webp`,
+        title: {en: 'Hills', uk: 'Пагорби', es: 'Colinas'},
+        src: hills,
         year: '2024',
-        material: { en: 'Oil Pastel', uk: 'Олійна пастель', es: 'Pastel al óleo' },
+        material: {en: 'Oil Pastel', uk: 'Олійна пастель', es: 'Pastel al óleo'},
         description: {
             en: `I found a painting kit for Japanese-style mountains at a store called "Aurora." However, following references is boring, so I decided to create something unique. \n\nLater, I saw a TikTok about this specific kit and posted my result in the comments—it surprisingly got over 500 likes!`,
             uk: `Я знайшла набір для малювання гір у японському стилі в магазині "Аврора". Проте дотримуватися референсів нудно, тому я вирішила створити щось унікальне. \n\nПізніше я побачила TikTok про цей самий набір і виклала свій результат у коментарях — він несподівано набрав понад 500 лайків!`,
@@ -116,10 +130,10 @@ export const galleryItems: GalleryItem[] = [
     },
     {
         id: '7',
-        title: { en: 'Japanese Cats', uk: 'Японські коти', es: 'Gatos japoneses' },
-        src: `${BASE_URL}gallery/Japanese Cats.webp`,
+        title: {en: 'Japanese Cats', uk: 'Японські коти', es: 'Gatos japoneses'},
+        src: cats,
         year: '2025',
-        material: { en: 'Acrylics', uk: 'Акрил', es: 'Acrílicos' },
+        material: {en: 'Acrylics', uk: 'Акрил', es: 'Acrílicos'},
         description: {
             en: `I wanted to paint something special for Darío since he loves Japan. I worked on this piece for over half a year before gifting it to him. \n\nThe coolest part? About six months later, we actually ended up visiting Japan together.`,
             uk: `Я хотіла намалювати щось особливе для Даріо, оскільки він любить Японію. Я працювала над цією роботою понад пів року, перш ніж подарувати її йому. \n\nНайкрутіше? Приблизно через шість місяців ми справді поїхали до Японії разом.`,
@@ -128,10 +142,10 @@ export const galleryItems: GalleryItem[] = [
     },
     {
         id: '8',
-        title: { en: 'Mark', uk: 'Марк', es: 'Mark' },
-        src: `${BASE_URL}gallery/Mark.webp`,
+        title: {en: 'Mark', uk: 'Марк', es: 'Mark'},
+        src: mark,
         year: '2023',
-        material: { en: 'Gouache', uk: 'Гуаш', es: 'Gouache' },
+        material: {en: 'Gouache', uk: 'Гуаш', es: 'Gouache'},
         description: {
             en: `This reminds me of a "golden time" in the dorm. We used to gather with friends, drink wine, and watch the show "Supermum" in the background while painting.`,
             uk: `Це нагадує мені про "золотий час" у гуртожитку. Ми збиралися з друзями, пили вино і дивилися шоу "Супермама" на фоні, поки малювали.`,
@@ -140,10 +154,10 @@ export const galleryItems: GalleryItem[] = [
     },
     {
         id: '9',
-        title: { en: 'Mushroom', uk: 'Гриб', es: 'Seta' },
-        src: `${BASE_URL}gallery/Mushroom.webp`,
+        title: {en: 'Mushroom', uk: 'Гриб', es: 'Seta'},
+        src: mushroom,
         year: '2022',
-        material: { en: 'Acrylics', uk: 'Акрил', es: 'Acrílicos' },
+        material: {en: 'Acrylics', uk: 'Акрил', es: 'Acrílicos'},
         description: {
             en: `It was my mum's birthday, and I didn't have a gift ready. I managed to finish this entire painting in just three days!`,
             uk: `Це був день народження моєї мами, а в мене не було готового подарунка. Мені вдалося закінчити всю цю картину всього за три дні!`,
@@ -152,10 +166,10 @@ export const galleryItems: GalleryItem[] = [
     },
     {
         id: '10',
-        title: { en: 'Panels', uk: 'Панельки', es: 'Paneles' },
-        src: `${BASE_URL}gallery/Panels.webp`,
+        title: {en: 'Panels', uk: 'Панельки', es: 'Paneles'},
+        src: panels,
         year: '2020-2021',
-        material: { en: 'Acrylics', uk: 'Акрил', es: 'Acrílicos' },
+        material: {en: 'Acrylics', uk: 'Акрил', es: 'Acrílicos'},
         description: {
             en: `This was painted a while ago, so I don't remember all the details. However, I loved the raw state of the painting so much that I decided never to "finish" it. \n\nIt remains one of my favorite pieces and still hangs on my wall today.`,
             uk: `Це було намальовано давно, тому я не пам'ятаю всіх деталей. Проте мені так сподобався цей незавершений стан картини, що я вирішила ніколи її не "доробляти". \n\nВона залишається однією з моїх улюблених робіт і досі висить на моїй стіні.`,
@@ -164,10 +178,10 @@ export const galleryItems: GalleryItem[] = [
     },
     {
         id: '11',
-        title: { en: 'Peach', uk: 'Персик', es: 'Melocotón' },
-        src: `${BASE_URL}gallery/Peach.webp`,
+        title: {en: 'Peach', uk: 'Персик', es: 'Melocotón'},
+        src: peach,
         year: '2021',
-        material: { en: 'Pencils', uk: 'Олівці', es: 'Lápices' },
+        material: {en: 'Pencils', uk: 'Олівці', es: 'Lápices'},
         description: {
             en: `This was completely random. While in Poland, I bought some colorful pencils and wanted to draw something but had no ideas. \n\nI asked my roommate for a random word, and she said: "Peach."`,
             uk: `Це було абсолютно випадково. Будучи в Польщі, я купила кольорові олівці й хотіла щось намалювати, але не мала ідей. \n\nЯ попросила сусідку по кімнаті сказати випадкове слово, і вона сказала: "Персик".`,
@@ -176,10 +190,10 @@ export const galleryItems: GalleryItem[] = [
     },
     {
         id: '12',
-        title: { en: 'Siauliai', uk: 'Шяуляй', es: 'Siauliai' },
-        src: `${BASE_URL}gallery/Siauliai.webp`,
+        title: {en: 'Siauliai', uk: 'Шяуляй', es: 'Siauliai'},
+        src: siauliai,
         year: '2022-2023',
-        material: { en: 'Acrylics', uk: 'Акрил', es: 'Acrílicos' },
+        material: {en: 'Acrylics', uk: 'Акрил', es: 'Acrílicos'},
         description: {
             en: `This was my first "long-term" project, taking over six months to complete. Most of the work was done during wine-and-paint nights with friends in the dorm. \n\nNow it belongs to a friend. Even though she moved from Šiauliai to Vilnius, this painting serves as a reminder of the place where we first met.`,
             uk: `Це був мій перший "довгостроковий" проєкт, на завершення якого пішло понад шість місяців. Більша частина роботи була зроблена під час вечорів з вином та малюванням з друзями в гуртожитку. \n\nТепер вона належить подрузі. Хоча вона переїхала з Шяуляя до Вільнюса, ця картина служить нагадуванням про місце, де ми вперше зустрілися.`,
@@ -188,10 +202,10 @@ export const galleryItems: GalleryItem[] = [
     },
     {
         id: '13',
-        title: { en: 'Tangerine', uk: 'Мандарин', es: 'Mandarina' },
-        src: `${BASE_URL}gallery/Tangerine.webp`,
+        title: {en: 'Tangerine', uk: 'Мандарин', es: 'Mandarina'},
+        src: tangerine,
         year: '2020',
-        material: { en: 'Acrylics', uk: 'Акрил', es: 'Acrílicos' },
+        material: {en: 'Acrylics', uk: 'Акрил', es: 'Acrílicos'},
         description: {
             en: `I believe I was following a tutorial for this one. I don't have many other memories associated with it, but I like how it turned out.`,
             uk: `Здається, я малювала це за туторіалом. У мене не дуже багато спогадів, пов'язаних з нею, але мені подобається результат.`,
@@ -200,10 +214,10 @@ export const galleryItems: GalleryItem[] = [
     },
     {
         id: '14',
-        title: { en: 'Yellow Taxi', uk: 'Жовте таксі', es: 'Taxi amarillo' },
-        src: `${BASE_URL}gallery/Yellow Taxi.webp`,
+        title: {en: 'Yellow Taxi', uk: 'Жовте таксі', es: 'Taxi amarillo'},
+        src: taxi,
         year: '2023-2024',
-        material: { en: 'Acrylics', uk: 'Акрил', es: 'Acrílicos' },
+        material: {en: 'Acrylics', uk: 'Акрил', es: 'Acrílicos'},
         description: {
             en: `One of my personal favorites. Cityscapes are what I enjoy painting the most. This piece took about half a year to finish, but the result was worth it.`,
             uk: `Одна з моїх найулюбленіших робіт. Міські пейзажі — це те, що мені подобається малювати найбільше. На цю роботу пішло близько пів року, але результат того вартий.`,
