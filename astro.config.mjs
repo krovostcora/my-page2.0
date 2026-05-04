2fea134f7f2051eb558306d90f6278e2db1af1aa
Anna Kutova <annnakutova@gmail.com>
2026-05-04 19:48:55 +0200

    chore: removed astrojs/tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  vite:{
+      plugins: [tailwindcss()],
+  },
   site: 'https://krovostcora.github.io/my-page2.0',
   base: '/my-page2.0/',
     // build: {
