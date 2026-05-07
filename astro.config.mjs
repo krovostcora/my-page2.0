2ff5e6914c512d963f06bde4592a24fc50898c05
Anna Kutova <annnakutova@gmail.com>
2026-05-07 23:26:05 +0200

    feat: astro add alpinejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import alpinejs from '@astrojs/alpinejs';
+
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), alpinejs()],
   site: 'https://krovostcora.github.io/my-page2.0',
   base: '/my-page2.0/',
     // build: {
